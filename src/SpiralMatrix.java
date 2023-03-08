b971b375bdb066fd02a8e9de73a535036791044e
yitong <yitongcool@foxmail.com>
2023-03-08 20:43:30 +0800

    upload solution

---
+++ src/SpiralMatrix.java
@@ -0,0 +1,89 @@
+package Spring2023A3;
+
+import java.util.Scanner;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt();
+        String s = sc.next();
+        String decompressed = decompress(s);
+        char[][] sm = spiralMatrix(decompressed, m, n);
+        printSpiralMatrix(sm);
+        sc.close();
+    }
+
+    public static String decompress(String s) {
+        StringBuilder b = new StringBuilder();
+        int index = 0;
+        while (index < s.length() - 1) {
+            char c = s.charAt(index);
+            StringBuilder numberFormatBuilder = new StringBuilder();
+            int i = index + 1;
+            while (i < s.length()) {
+                if (Character.isDigit(s.charAt(i))) {
+                    numberFormatBuilder.append(s.charAt(i));
+                    i++;
+                } else {
+                    break;
+                }
+            }
+
+            index = i;
+            int repeatNumber = Integer.parseInt(numberFormatBuilder.toString());
+            b.append(String.valueOf(c).repeat(Math.max(0, repeatNumber)));
+        }
+
+        return b.toString();
+    }
+
+    public static final int[][] DIRECTION_ARRAY = {
+            {0, 1},
+            {-1, 0},
+            {0, -1},
+            {1, 0}
+    };
+
+    private static boolean outOfBoundary(char[][] matrix, int m, int n, int x, int y) {
+        return !(0 <= x && x < m && 0 <= y && y < n);
+    }
+
+    private static boolean notEmpty(char[][] matrix, int x, int y){
+        return matrix[x][y] != '\u0000';
+    }
+
+    public static char[][] spiralMatrix(String decompressed, int m, int n) {
+        char[][] matrix = new char[m][n];
+        int x = m - 1, y = 0;
+        int directionIndex = 0;
+        for (char c : decompressed.toCharArray()) {
+            matrix[x][y] = c;
+
+            int[] currentDirection = DIRECTION_ARRAY[directionIndex];
+            int repeatTimes = 0;
+            while (outOfBoundary(matrix, m, n, x + currentDirection[0], y + currentDirection[1])
+            || notEmpty(matrix, x+ currentDirection[0], y+ currentDirection[1])) {
+                directionIndex = (directionIndex + 1) % DIRECTION_ARRAY.length;
+                currentDirection = DIRECTION_ARRAY[directionIndex];
+                repeatTimes++;
+                if(repeatTimes >= DIRECTION_ARRAY.length){
+                    break;
+                }
+            }
+
+            x = x + currentDirection[0];
+            y = y + currentDirection[1];
+        }
+
+        return matrix;
+    }
+
+    public static void printSpiralMatrix(char[][] matrix) {
+        for (char[] cs : matrix) {
+            for (char c : cs) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
